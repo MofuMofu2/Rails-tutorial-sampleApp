3947ddcc1ad46d9cbfffa75829fd6620d77dcccc
MofuMofu2 <froakie002@gmail.com>
2018-06-07 17:19:55 +0900

    add: nameを苗字わけて入れられるようにする

--- example_user.rb
+++ example_user.rb
@@ -1,12 +1,14 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :first_name, :last_name, :email
 
   def initialize(attributes = {})
-    @name = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
+  
 
   def formatted_email
-    "#{@name}<#{@email}>"
+    "#{@full_name}<#{@email}>"
   end
 end
