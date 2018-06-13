691e7ebc2fcf665841cffb9cae528ab5e3e1b659
MofuMofu2 <froakie002@gmail.com>
2018-06-13 11:37:29 +0900

    fix:取得したいURLとパスの整合性が取れていなかったため修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,26 +12,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get help_path
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
-
   test "should get help" do
-    get about_path
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get contact_path
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
