cfa9221af426d13910b3c9f72f7b78191fbdb1f7
MofuMofu2 <froakie002@gmail.com>
2018-07-03 16:46:53 +0900

    [add] メールアドレスを利用してDBに問い合わせを行う

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      render 'new'
+    end
   end
   
   def destroy
