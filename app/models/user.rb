44cfb9cabf8b2831ce7c53634ba54f7d0dbd0020
MofuMofu2 <froakie002@gmail.com>
2018-07-05 15:01:52 +0900

    [add] rememberメソッドをUserモデルに追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -16,4 +17,8 @@ class User < ApplicationRecord
     def User.new_token
         SecureRandom.urlsafe_base65
     end
+    
+    def remember
+        self.:remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
 end
