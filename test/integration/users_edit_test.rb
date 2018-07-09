e0a2fd8e4a1ebcb8adda201e73ea62e2fa35332f
MofuMofu2 <froakie002@gmail.com>
2018-07-09 16:49:18 +0900

    [fix] emailの変数が定義できていなかったので修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,7 +17,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
-    email user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" }}
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" }}
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
