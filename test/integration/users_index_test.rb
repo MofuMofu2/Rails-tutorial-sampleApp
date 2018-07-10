85474782c731523af404b01d4aa391c61b2575a2
MofuMofu2 <froakie002@gmail.com>
2018-07-10 21:40:39 +0900

    [add] 管理者権限ではユーザーが削除でき、そうでなければ削除できない

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,7 +12,19 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), test: user.name        
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+  
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
