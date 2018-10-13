11fb02b423db825da122f41edc506f326fd0c590
MofuMofu2 <froakie002@gmail.com>
2018-10-13 14:56:31 +0900

    [update] 標準出力ファイルは無視するように設定
    セキュリティグループは複数で指定しないとエラーになる。多分指定する方法が違うのかな〜〜

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,5 @@
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
-variable "aws_security_group" {}
 variable "region" {
   default = "ap-northeast-1"
 }
@@ -14,7 +13,6 @@ provider "aws" {
 resource "aws_instance" "hello_world_terraform" {
   ami           = "ami-04d3eb2e1993f679b"
   instance_type = "t2.micro"
-  security_groups = "${var.aws_security_group}"
   tags {
     Name = "rails-tutorial-application"
   }
